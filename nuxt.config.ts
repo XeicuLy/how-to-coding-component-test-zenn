4784db60b6c2deb6873f4d144bc91e5b2f73f843
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-04-13 17:10:01 +0900

    🔧 : Vitestを設定する
    - パッケージ追加
    - 設定ファイル追加
    - npm scripts登録
    - ESLint設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   srcDir: 'src/',
-  modules: ['@nuxt/eslint', '@nuxt/test-utils'],
+  modules: ['@nuxt/eslint', '@nuxt/test-utils/module'],
 });
