b42d25f561ad70c5a0b9d93408f7644fb5566b86
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-04-27 21:40:40 +0900

    ✅ : イベントテストを実装

---
+++ src/components/chapter-9/TestComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { additionNumber } from '@/utils/chapter-9/test';
+
+const isClicked = ref(false);
+
+const handleClick01 = () => {
+  additionNumber(1, 2);
+};
+
+const handleClick02 = () => {
+  isClicked.value = !isClicked.value;
+};
+</script>
+<template>
+  <button data-testid="button01" @click="handleClick01">Button</button>
+  <button data-testid="button02" @click="additionNumber(3, 3)">Button</button>
+  <button data-testid="button03" @click="handleClick02">Button</button>
+</template>
